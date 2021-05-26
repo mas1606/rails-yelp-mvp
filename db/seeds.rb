3b20f2ef299f134f32609895e0378f24277018f3
Marcio Silva <mas16_06@hotmail.com>
2021-05-26 14:40:55 -0300

    My fist project using scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+fasano = { name: "Trattoria Fasano", address: "Rua Iguatemi, s/n", category: "italian" }
+pizza =  { name: "Nonno Paolo", address: "Av. Abilio Soares, 830", category: "italian"}
+china =  { name: "China in Box", address: "Av. Henrique Schaumman, 500", category: "chinese"}
+frances =  { name: "Olimpe", address: "Lagoa Rodrigo de Freitas", category: "french"}
+belga =  { name: "Chez Vouz", address: "Av Lavandisca", category: "belgian"}
+
+[ fasano, pizza, china, frances, belga ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
